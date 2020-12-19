1df7efcb57b8a19da877c35d3fe61c43a997afe8
amin lahiai <aminlahiani@outlook.com>
2020-12-19 20:57:06 +0100

    add categoryRouter in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import productRouter from './routes/product';
 import userRouter from './routes/users';
-
+import categoryRouter from './routes/category';
 const app = express();
 
 app.use(express.json());
@@ -10,5 +10,5 @@ app.get('/', (req, res) => res.send('hello'));
 
 app.use('/api/users', userRouter);
 app.use('/api/product', productRouter)
-
+app.use('/api/category', categoryRouter)
 export default app;
