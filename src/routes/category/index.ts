cf859fb64046c2caee3b6c61d40c1d4c6ca3e8a0
amin lahiai <aminlahiani@outlook.com>
2020-12-19 21:43:46 +0100

    add Router

--- src/routes/category/index.ts
+++ src/routes/category/index.ts
@@ -1,7 +1,9 @@
+
+import { createCategory } from '../../controllers/category/createcCategory';
 import express from "express";
 
 const categoryRouter = express.Router();
 
-categoryRouter.post('/', create);
+categoryRouter.post('/', createCategory);
 
 export default categoryRouter ; 
