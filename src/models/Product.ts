d7d9a499434e225713d8a21c5c822b41a6683788
amin lahiai <aminlahiani@outlook.com>
2020-12-19 16:22:52 +0100

    CategoryModel

---
+++ src/models/Product.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+
+
+
+interface ProductsAttrs {
+    
+	name: string;
+	description: string ;
+	price: string ;
+	stock_count: string;
+	image : boolean;
+	category: string ;
+
+}
