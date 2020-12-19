fd517690af231a2df40576354531fa4ae660b0b3
amin lahiai <aminlahiani@outlook.com>
2020-12-19 17:52:43 +0100

    add  productModel

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -3,13 +3,51 @@ import mongoose from "mongoose"
 
 
 
-interface ProductsAttrs {
+interface ProductAttrs {
     
 	name: string;
 	description: string ;
-	price: string ;
-	stock_count: string;
-	image : boolean;
+	price: number ;
+	stock_count: number;
+	image : string,
 	category: string ;
 
 }
+
+interface ProductModel extends mongoose.Model<ProductDoc> {
+	build(attrs: ProductAttrs): ProductDoc;
+}
+interface ProductDoc extends mongoose.Document {
+	name: string;
+	description: string ;
+	price: number ;
+	stock_count: number;
+	image : string,
+	category: string ;
+
+}
+const productSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		description: { type: String , required: true },
+		price: { type: Number, required: true, },
+		stock_count:  { type: Number, required: true},
+		image: { type: String, required: true },
+		category: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'Category',
+		  },
+
+	},
+	{
+		timestamps: true,
+	}
+);
+
+productSchema.statics.build = (attrs: ProductAttrs) => {
+	return new Product(attrs);
+};
+
+const Product = mongoose.model<ProductDoc, ProductModel>('Product', productSchema);
+export default Product;
