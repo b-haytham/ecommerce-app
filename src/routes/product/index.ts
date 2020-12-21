f89f5c2607e33b8a9178de20e31dd2722477ae10
amin lahiai <aminlahiani@outlook.com>
2020-12-21 19:24:37 +0100

    add product routes

--- src/routes/product/index.ts
+++ src/routes/product/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { createProduct } from "../../controllers/products/createProduct";
+import { deleteProduct } from "../../controllers/products/deleteProduct";
+import { getProduct, getProductID } from "../../controllers/products/getProduct";
+import { updateProduct } from "../../controllers/products/updateProduct";
 
 
 const productRouter = express.Router();
 
 productRouter.post('/' , createProduct);
+productRouter.get('/', getProduct);
+productRouter.get('/:id', getProductID)
+productRouter.put('/:id', updateProduct);
+productRouter.delete('/:id', deleteProduct);
 
 export default productRouter ; 
