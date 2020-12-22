6ca4851b941da6f4a08691b9a8582fb977e22b3c
amin lahiai <aminlahiani@outlook.com>
2020-12-22 21:50:22 +0100

    authuser

--- src/controllers/users/authUser.ts
+++ src/controllers/users/authUser.ts
@@ -13,25 +13,30 @@ export const authUser = asyncHandler(async (req, res) => {
    	// validator error comming from req.body
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			res.status(404).json({ errors: errors.array() })
-			return res.redirect("/login");
-		}
-    const { email, password } = req.body
-  try {
-    const user = await User.findOne({ email })
+			 return res.status(404).json({ errors: errors.array() })
+			
+    }
     
-    if (user && (await user.matchesPassword(password))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      })
-  }} catch (error) {
-    console.log(error)
-			res.json(error);
-  }/*
+  
+      const { email, password } = req.body
+
+      const user = await User.findOne({ email })
+  
+      if (user && (await user.matchesPassword(password))) {
+        res.json({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        })
+    } else {
+      res.status(401)
+      throw new Error('Invalid email or password')
+    
+    }
+  })
+/*
     const user = await User.findOne({ email })
     
           
@@ -46,4 +51,4 @@ export const authUser = asyncHandler(async (req, res) => {
     } else {
       res.status(401)
       throw new Error('Invalid email or password')*/
-    })
+  
