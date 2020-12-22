9714aec96199b0cbfe4482f8f58e9701e7a54855
amin lahiai <aminlahiani@outlook.com>
2020-12-22 13:18:51 +0100

    createUser Logic

--- src/controllers/users/createUser.ts
+++ src/controllers/users/createUser.ts
@@ -1,39 +1,75 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserRoles } from '../../models/types';
 import User from '../../models/User';
+import { check, validationResult } from "express-validator";
+import generateToken from '../../util/generateToken';
 
-export const createUser = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	const { username, email, password } = req.body;
 
-	const isUserExist = await User.findOne({ email });
+export const createUser =
+	async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
 
-	if (isUserExist) {
-		return res
-			.status(400)
-			.json({ error: { message: 'User already exist' } });
-	}
+		await check("email", "Email is not valid").isEmail().run(req);
+		await check("password", "Password cannot be blank").isLength({ min: 5 }).run(req);
 
-	const user = User.build({
-		username,
-		first_name: null,
-		last_name: null,
-		email,
-		password,
-		shipping_address: null,
-		role: UserRoles.USER,
-		address: null,
-		phone: null,
-		city: null,
-		country: null,
-		postal_code: null,
-		isVerified: false,
-	});
+		// validator error comming from req.body
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(404).json({ errors: errors.array() })
+			return res.redirect("/login");
+		}
+		const { username, email, password } = req.body;
 
-	await user.save();
 
-	res.status(201).json(user);
-};
+		try {
+			const user = User.build({
+				username,
+				first_name: null,
+				last_name: null,
+				email,
+				password,
+				shipping_address: null,
+				role: UserRoles.USER,
+				address: null,
+				phone: null,
+				city: null,
+				country: null,
+				postal_code: null,
+				isVerified: false,
+
+			});
+			await user.save();
+
+			if (user) {
+				res.status(201).json({
+					_id: user._id,
+					username: user.username,
+					email: user.email,
+
+					token: generateToken(user._id),
+				})
+			} else {
+				res.status(400)
+				throw new Error('Invalid user data')
+
+
+
+
+
+
+
+			}
+		} catch (error) {
+			console.log(error)
+			res.json(error);
+
+		}
+
+
+
+
+
+	};
