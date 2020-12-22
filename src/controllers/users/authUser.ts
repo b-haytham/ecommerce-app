abb37df8482b582fed0ce8d7678bfaf2b0ab1d29
amin lahiai <aminlahiani@outlook.com>
2020-12-22 21:50:50 +0100

    authuser

--- src/controllers/users/authUser.ts
+++ src/controllers/users/authUser.ts
@@ -36,19 +36,5 @@ export const authUser = asyncHandler(async (req, res) => {
     
     }
   })
-/*
-    const user = await User.findOne({ email })
-    
-          
-    if (user && (await user.matchesPassword(password))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      })
-    } else {
-      res.status(401)
-      throw new Error('Invalid email or password')*/
+
   
