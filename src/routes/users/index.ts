3fa9caa21feed9c8723da6ce88bd06810f7a486d
amin lahiai <aminlahiani@outlook.com>
2020-12-22 14:16:41 +0100

    add routes authUser

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { authUser } from '../../controllers/users/authUser';
 import { createUser } from '../../controllers/users/createUser';
 
 
 const userRouter = express.Router();
 
 userRouter.post('/', createUser);
+userRouter.post('/login', authUser);
 
 export default userRouter;
